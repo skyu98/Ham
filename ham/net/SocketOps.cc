362dfcc175a062cce7845ba960a412e91cd8a4cb
yuxiao <2286229152@qq.com>
2021-02-02 11:07:31 +0800

    SocketOps

--- ham/net/SocketOps.cc
+++ ham/net/SocketOps.cc
@@ -1,41 +1,65 @@
 #include "SocketOps.h"
 #include "base/Log.h"
-using namespace ham::net;
+#include "base/Types.h"
 
+namespace ham
+{
+namespace net
+{
+namespace sockets
+{
+
+#if VALGRIND || defined (NO_ACCEPT4)
+void setNonBlockAndCloseOnExec(int sockfd) {
+    int flags = ::fcntl(sockfd, F_GETFL, 0);
+    flags |= O_NONBLOCK;    
+    int res = ::fcntl(sockfd, F_SETFL, flags);
+
+    flags = ::fcntl(sockfd, F_GETFD, 0);
+    flags |= FD_CLOEXEC;
+    ret = ::fcntl(sockfd, F_SETFD, flags);
+}
+
+#endif
 
 // Socket functions
-int socket::socket(){
-	int sockfd = 0;
-	// sockfd = ::socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, IPPROTO_TCP);
-	sockfd = ::socket(AF_INET, SOCK_STREAM, 0);
+int sockets::createNonblockingOrDie(){
+#if VALGRIND	
+    int sockfd = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (sockfd < 0)
     {
-        ERROR("sockets::createNonblockingOrDie");
+        CRITICAL("sockets::createNonblockingOrDie");
     }
+    setNonBlockAndCloseOnExec(sockfd);
+#else
+    int sockfd = ::socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, IPPROTO_TCP);
+    if (sockfd < 0)
+    {
+        CRITICAL("sockets::createNonblockingOrDie");
+    }
+#endif
 	return sockfd;
 }
 
-int socket::bind(int sockfd, const struct sockaddr_in& address){
-	int ret = 0;
-	//ret = bind(sockfd, static_cast<SA*>((void*)&address), sizeof(address));
-	if(ret == -1)
+void sockets::bindOrDie(int sockfd, const sockets::SA_in& address){
+	int ret = bind(sockfd, sockaddr_cast(&address),
+                static_cast<socklen_t>(sizeof(address)));
+	if(ret < 0)
 	{
-		ERROR("bind error");
+		ERROR("sockets::bindOrDie error");
 	}
-	return ret;
 };
 
-int socket::listen(int socket, int backlog){
-	int ret = 0;
-	ret = listen(socket, backlog);
-	if(ret == -1)
+void sockets::listenOrDie(int socket)
+{
+	int ret = ::listen(socket, SOMAXCONN);
+	if(ret < 0)
 	{
-		ERROR("listen error");
+		ERROR("sockets::listenOrDie error");
 	}
-	return ret;
 }
 
-ssize_t socket::read(int fd, void *buf, size_t nbyte){
+ssize_t sockets::read(int fd, void *buf, size_t nbyte){
 	ssize_t ret = 0;
 
 again:
@@ -54,7 +78,7 @@ again:
 	return ret;
 }
 
-ssize_t socket::write(int fd, void *buf, size_t nbyte){
+ssize_t sockets::write(int fd, void *buf, size_t nbyte){
 	ssize_t ret = 0;
 
 again:
@@ -71,6 +95,60 @@ again:
 		}
 	}
 	return ret;
+}
+
+const SA* sockaddr_cast(const SA_in* addr) 
+{
+    return static_cast<const SA*>(implicit_cast<const void*>(addr));
+}
+
+SA* sockaddr_cast(SA_in* addr) 
+{
+    return static_cast<SA*>(implicit_cast<void*>(addr));
+}
+
+const SA_in* sockaddr_in_cast(const SA* addr) 
+{
+    return static_cast<const SA_in*>(implicit_cast<const void*>(addr));
+}
+
+SA_in* sockaddr_in_cast(SA* addr) 
+{
+    return static_cast<SA_in*>(implicit_cast<void*>(addr));
+}
+
+std::string sockets::toIpStr(const SA_in* addr) {
+    char buf[32];
+    ::inet_ntop(AF_INET, &addr->sin_addr, buf, static_cast<socklen_t>(sizeof(buf)));
+    return buf;
+}
+
+void sockets::close(int sockfd) {
+    if(::close(sockfd) < 0) {
+        ERROR("sockets::close");
+    }
+}
+
+void sockets::shutdownWrite(int sockfd) {
+    if(::shutdown(sockfd, SHUT_WR) < 0) {
+        ERROR("sockets::shutdownWrite");
+    }
+}
+
+SA_in sockets::getLocalAddr(int sockfd) {
+    SA_in localaddr;
+    bzero(&localaddr, sizeof(localaddr));
+    socklen_t addrlen = static_cast<socklen_t>(sizeof(localaddr));
+    if(::getsockname(sockfd, sockaddr_cast(&localaddr), &addrlen) < 0) {
+        ERROR("sockets::getLocalAddr");
+    }
+    return localaddr;
+}
+
+
+
+}
+}
 }
 
 
--- ham/net/SocketOps.h
+++ ham/net/SocketOps.h
@@ -9,26 +9,37 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <iostream>
+#include <string>
 
 namespace ham
 {
 namespace net
 {
-namespace socket
+namespace sockets
 {
 
 typedef struct sockaddr SA;
+typedef struct sockaddr_in SA_in;
 
-//void print_err(const char* str);
+const SA* sockaddr_cast(const SA_in* addr);
+SA* sockaddr_cast(SA_in* addr);
+const SA_in* sockaddr_in_cast(const SA* addr);
+SA_in* sockaddr_in_cast(SA* addr);
 
-int socket();
-
-int bind(int sockfd, const struct sockaddr_in& address);
-
-int listen(int socket, int backlog = 128);
+int createNonblockingOrDie();
+void bindOrDie(int sockfd, const SA_in& addrin);
+void listenOrDie(int socket);
+int  accept(int sockfd, SA_in& addrin);
 
 ssize_t read(int fd, void *buf, size_t nbyte);
 ssize_t write(int fd, void *buf, size_t nbyte);
+ssize_t write(int fd, std::string msg);
+
+void close(int sockfd);
+void shutdownWrite(int sockfd);
+void ipPortToAddrin(const std::string& ip, uint16_t port, struct sockaddr_in* addr);
+std::string toIpStr(const struct sockaddr_in* addr);
+struct sockaddr_in getLocalAddr(int sockfd);
 
 }
 }
