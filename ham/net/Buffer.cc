80a68056b8e33dfb17b8491c869c714288786946
yuxiao <2286229152@qq.com>
2021-02-16 14:25:46 +0800

    Buffer

--- ham/net/Buffer.cc
+++ ham/net/Buffer.cc
@@ -1,6 +1,8 @@
 #include "net/Buffer.h"
 #include "net/Endian.h"
 #include <assert.h>
+#include <cstring>
+
 namespace ham
 {
     namespace net
@@ -17,59 +19,82 @@ namespace ham
             assert(prependableBytes() == kCheapPrepend);
         }
         
-        void Buffer::swap(const Buffer& rhs) 
+        void Buffer::swap(Buffer& rhs) 
         {
-            
+            std::swap(buffer_, rhs.buffer_);
+            std::swap(readIndex, rhs.readIndex);
+            std::swap(writeIndex, rhs.writeIndex);
         }
         
         int8_t Buffer::peekInt8() const
         {
-            
+            assert(readableBytes() >= sizeof(int8_t));
+            int8_t num = *peek();  // one byte, no need to NetWorkToHost
+            return num;
         }
         
         int16_t Buffer::peekInt16() const
         {
-            
+            assert(readableBytes() >= sizeof(int16_t));
+            int16_t be16 = 0;
+            ::memcpy(&be16, peek(), sizeof(int16_t));
+            return sockets::networkToHost16(be16);
         }
         
         int32_t Buffer::peekInt32() const
         {
-            
+            assert(readableBytes() >= sizeof(int32_t));
+            int16_t be32 = 0;
+            ::memcpy(&be32, peek(), sizeof(int32_t));
+            return sockets::networkToHost16(be32);
         }
         
         int32_t Buffer::peekInt64() const
         {
-            
+            assert(readableBytes() >= sizeof(int64_t));
+            int16_t be64 = 0;
+            ::memcpy(&be64, peek(), sizeof(int64_t));
+            return sockets::networkToHost16(be64);
         }
         
-        int8_t Buffer::readInt8() const
+        int8_t Buffer::readInt8() 
         {
-            
+            int8_t res = peekInt8();
+            retrieveInt8();
+            return res;
         }
         
-        int16_t Buffer::readInt16() const
+        int16_t Buffer::readInt16() 
         {
-            
+            int16_t res = peekInt16();
+            retrieveInt16();
+            return res;
         }
         
-        int32_t Buffer::readInt32() const
+        int32_t Buffer::readInt32() 
         {
-            
+            int32_t res = peekInt32();
+            retrieveInt32();
+            return res;
         }
         
-        int32_t Buffer::readInt64() const
+        int32_t Buffer::readInt64() 
         {
-            
+            int64_t res = peekInt64();
+            retrieveInt64();
+            return res;
         }
         
         void Buffer::append(const char* data, size_t len) 
         {
-            
+            ensureWritableBytes(len);
+            std::copy(data, data + len, beginWrite());
+            hasWritten(len);
         }
         
         void Buffer::append(const std::string& str) 
         {
-            
+            append(str.data(), str.size());
         }
     }
 }
--- ham/net/Buffer.h
+++ ham/net/Buffer.h
@@ -19,9 +19,8 @@ public:
     static const  size_t kInitialSize = 1024;
     static const  size_t kCheapPrepend = 8;
     static const char kCRLF[];
-
     Buffer();
-    void swap(const Buffer& rhs);
+    void swap(Buffer& rhs);
     
     size_t readableBytes() const { return writeIndex - readIndex ;}
     size_t writableBytes() const { return buffer_.size() - writeIndex; }
@@ -34,10 +33,10 @@ public:
     int32_t peekInt32() const;
     int32_t peekInt64() const;
 
-    int8_t readInt8() const;
-    int16_t readInt16() const;
-    int32_t readInt32() const;
-    int32_t readInt64() const;
+    int8_t readInt8();
+    int16_t readInt16();
+    int32_t readInt32();
+    int32_t readInt64();
 
     // move the readIndex (after read)
     void retrieve(size_t len);
@@ -46,6 +45,9 @@ public:
     void retrieveInt32() { retrieve(sizeof(int32_t)); }
     void retrieveInt64() { retrieve(sizeof(int64_t)); }
 
+    void ensureWritableBytes(size_t len);
+    char* beginWrite() { return begin() + writeIndex; }
+    const char* beginWrite() const { return begin() + writeIndex; }
     void hasWritten(size_t len) { writeIndex += len; }
     void append(const char* data, size_t len);
     void append(const std::string& str);
@@ -54,6 +56,8 @@ private:
     // 注意到这里，典型的const重载
     char* begin() { return &(*buffer_.begin()); }
     const char* begin() const { return &(*buffer_.begin()); }
+
+    void makeSpace(size_t len);
     
 };
 
