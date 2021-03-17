493d013a9d63bba23964942db2736d3710eebd46
yuxiao <2286229152@qq.com>
2021-03-17 19:49:18 +0800

    echo

---
+++ examples/simple/echo/main.cc
@@ -0,0 +1,45 @@
+#include "net/TcpServer.h"
+#include "net/EventLoop.h"
+#include "net/InetAddress.h"
+#include "net/TcpConnection.h"
+#include "net/Buffer.h"
+#include <iostream>
+
+using namespace ham;
+using namespace ham::net;
+
+void connectionCallback(const TcpConnectionPtr& conn)
+{
+    if(conn->isConnected())
+    {
+        std::cout << "New conn comes from " << conn->peerAddress().getIpPortStr()
+             << " and its name is " << conn->getName() << std::endl;
+    }
+    else
+    {
+        std::cout << "Conn " << conn->getName() << " is down..." << std::endl;
+    }
+}
+
+void msgCallback(const TcpConnectionPtr& conn, 
+                Buffer& buf, Timestamp receiveTime)
+{
+    std::string msg(buf.retrieveAllAsString());
+    std::cout << "New message from " << conn->getName() << ":" << msg << std::endl;
+    conn->send(msg);
+}                                
+
+int main()
+{
+    EventLoop loop;
+    InetAddress listenAddr(8888);
+    TcpServer server(&loop, listenAddr, "TcpServer");
+
+    server.setConnectionCallback(connectionCallback);
+    server.setMessageCallback(msgCallback);
+
+    server.start();
+
+    loop.loop();
+    return 0;
+}
